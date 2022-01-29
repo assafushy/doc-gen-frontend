a20bb7288070d80017e5b1c2fbd49242e7d52e6e
Assaf <assafushy@gmail.com>
2022-01-29 21:56:49 +0200

    docker file and script enhancments

--- src/deployment/env-uri-init.sh
+++ src/deployment/env-uri-init.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 echo replace MINIO-ENDPOINT-PLACEHOLDER
 find '/usr/share/nginx/html/static/js' -name '*.js' -exec sed -i 's,MINIO-ENDPOINT-PLACEHOLDER,'"${MINIO-ENDPOINT-PLACEHOLDER}"',g' {} \;
 
@@ -19,5 +17,5 @@ echo replace AWS-REGION-PLACEHOLDER
 find '/usr/share/nginx/html/static/js' -name '*.js' -exec sed -i 's,AWS-REGION-PLACEHOLDER,'"${AWS-REGION-PLACEHOLDER}"',g' {} \;
       
 echo starting server
-nginx -g daemon off;
+nginx -g "daemon off;"
 
