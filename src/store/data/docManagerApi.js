43842e05504097fb9010a76624096f0ea3b7f49d
amirlandau <amirlandau5@gmail.com>
2023-07-04 10:52:56 +0300

    change back the codManagerApi

--- src/store/data/docManagerApi.js
+++ src/store/data/docManagerApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import C from "../constants";
 import { v4 as uuidv4 } from "uuid";
-import { promisify } from "util";
 
 var Minio = require("minio");
 
@@ -12,11 +11,11 @@ const headers = {
   },
 };
 
-export const getBucketFileList = async (bucketName, isExternalUrl = false) => {
+export const getBucketFileList = async (bucketName,isExternalUrl = false) => {
   let url;
   try {
     url = `${C.jsonDocument_url}/minio/bucketFileList/${bucketName}?isExternalUrl=${isExternalUrl}`;
-    let res = await makeRequest(url, undefined, undefined, headers);
+    let res = await makeRequest(url, undefined,undefined,headers);
     return res.bucketFileList;
   } catch (e) {}
 };
@@ -25,7 +24,7 @@ export const getJSONContentFromFile = async (bucketName, fileName) => {
   let url;
   try {
     url = `${C.jsonDocument_url}/minio/ContentFromFile/${bucketName}/${fileName}`;
-    let res = await makeRequest(url, undefined, undefined, headers);
+    let res = await makeRequest(url, undefined,undefined,headers);
     return res.contentFromFile;
   } catch (e) {}
 };
@@ -35,62 +34,9 @@ export const createIfBucketDoesentExsist = async (bucketName) => {
   let data = { bucketName };
   try {
     url = `${C.jsonDocument_url}/minio/createBucket`;
-    let response = await makeRequest(url, "post", data, headers);
-
-    // Instantiate the minio client with the endpoint and access keys
-    var minioClient = new Minio.Client({
-        endPoint: 'localhost',
-        port: 9000,
-        useSSL: false,  // if you have not enabled https, set useSSL as false
-        accessKey: 'your-root-user',
-        secretKey: 'your-root-password'
-    });
-
-    // Define the lifecycle policy
-    const lifecycleConfig = {
-        Rule: [{
-            "ID": "Expire after one day",
-            "Status": "Enabled",
-            "Filter": {
-                "Prefix": "",
-            },
-            "Expiration": {
-                "Days": 1,
-            }
-        }]
-    };
-
-    // Promisify the getBucketLifecycle and setBucketLifecycle functions
-    const getBucketLifecyclePromise = promisify(minioClient.getBucketLifecycle).bind(minioClient);
-    const setBucketLifecyclePromise = promisify(minioClient.setBucketLifecycle).bind(minioClient);
-
-    let existingLifecycleConfig;
-
-    try {
-        // Try to get the existing lifecycle configuration
-        existingLifecycleConfig = await getBucketLifecyclePromise(bucketName);
-    } catch (err) {
-        // Handle specific error for no lifecycle configuration
-        if (err.code === 'NoSuchLifecycleConfiguration') {
-            console.log(`No lifecycle policy found for bucket '${bucketName}'. Setting up a new one.`);
-            await setBucketLifecyclePromise(bucketName, lifecycleConfig);
-        } else {
-            // Log other errors
-            console.error(err);
-        }
-    }
-
-    // If there is a lifecycle configuration, it's left unchanged
-    if (existingLifecycleConfig) {
-        console.log(`Lifecycle policy already exists for bucket '${bucketName}'. Not making any changes.`);
-    }
-
-    return response;
-  } catch (e) {
-    console.error(e);
-  }
+    return await makeRequest(url, "post", data, headers);
+  } catch (e) {}
 };
-
 
 const makeRequest = async (
   url,
